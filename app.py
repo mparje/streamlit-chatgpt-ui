63d33dff29b7efe47899fdf64a93035e328d354f
mparje <126131806+mparje@users.noreply.github.com>
2023-04-06 19:49:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ container = st.container()
 
 with container:
     with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("You:", key='input', height=100)
+        user_input = st.text_area("Title of your paper or article:", key='input', height=40)
         submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
