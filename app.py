c08feea2c8e77c8c865203bbaaa7733c9469c907
mparje <126131806+mparje@users.noreply.github.com>
2023-04-06 20:05:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ if clear_button:
     st.session_state['total_tokens'] = []
 
 # Upload a DOCX file
-uploaded_file = left_column.file_uploader("- Upload a document with quotes from various articles on the topic you are researching, including the reference in APA format (max. 2000 words).         Please note the ethical considerations when using this tool:
+uploaded_file = left_column.file_uploader("
+        - Upload a document with quotes from various articles on the topic you are researching, including the reference in APA format (max. 2000 words).         
+        Please note the ethical considerations when using this tool:
         - The generated text is meant to be a suggestion and should not replace your own creative work.
         - Verify the accuracy of the generated content, including citations, before using it in your research or academic work.
         Additional information:
